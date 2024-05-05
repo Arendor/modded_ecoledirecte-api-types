46df8008cd060de84255eeb87658aa8a40966200
Arendor <arendor.pro@gmail.com>
2024-05-05 15:04:54 +0200

    Add A2F types responses

--- v3/responses/login/index.ts
+++ v3/responses/login/index.ts
@@ -3,7 +3,35 @@ import { account } from "./accounts";
 
 export * from "./accounts";
 
-export type loginRes = loginResSuccess | failureRes;
+export type base64 = string
+export type loginRes = loginResSuccess | failureRes | A2FRes
+
+export type A2FRes = {
+	code: 250,
+	token: string,
+	message: 'Identifiant et/ou mot de passe invalide !',
+	data: { changementMDP: false, accounts: [] }
+}
+
+export type A2FQCM = {
+    code: 200,
+    data: {
+		question: base64, 
+        propositions: base64[]
+    },
+    message: null, // ???
+    host: "HTTP<n° serveur>"
+}
+
+export type A2FQCMRes = {
+    code: 200,
+    data: {
+		cn: base64, 
+        cv: base64,
+    },
+    message: null, // ???
+    host: "HTTP<n° serveur>"
+}
 
 export type loginResSuccess = {
 	code: 200;
